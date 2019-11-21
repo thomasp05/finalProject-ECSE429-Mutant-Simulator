5f012b9da3bd1d63d638b02f8820d13156179015
Thomas Philippon <thomas.philippon@mail.mcgill.ca>
2019-11-21 12:26:03 -0500

    First commit

---
+++ assignment1Q4.py
@@ -0,0 +1,32 @@
+__author__ = "Thomas Philippon"
+__ID__ = "260747645"
+__email__ = "thomas.philippon@mail.mcgill.ca"
+
+
+mutantList = ["+", "-", "*", "/"] #list of possible mutants
+nbMutant = [0,0,0,0]  #list containing the total number of mutants for each type
+output = open("mutantLibrary.txt", "w") #Output file containing the information about the mutants
+sut = open("sut.txt") #Software Under Test.
+lineCounter = 0 #line number
+
+for line in sut:
+    lineCounter = lineCounter + 1
+    for char in line:
+        if char =='+' or char =='-' or char =='*' or char =='/':
+            lineNumber = str(lineCounter)
+            output.write( lineNumber + ": " + str(char) +  ", ")
+            for i in mutantList:
+                if i != char:
+                    output.write(str(i)+ ", ")
+                    j = mutantList.index(i) 
+                    nbMutant[j] = nbMutant[j] +1 
+            output.write("\n")
+
+#Print the information about the total number of mutants of each type
+output.write("Number of mutants of type + : " + str(nbMutant[0]) + "\n")
+output.write("Number of mutants of type - : " + str(nbMutant[1]) + "\n")
+output.write("Number of mutants of type * : " + str(nbMutant[2]) + "\n")
+output.write("Number of mutants of type / : " + str(nbMutant[3]) + "\n")
+output.close()
+
+
---
+++ assignment2Q4.py
@@ -0,0 +1,90 @@
+__author__ = "Thomas Philippon"
+__ID__ = "260747645"
+__email__ = "thomas.philippon@mail.mcgill.ca"
+
+import os
+from shutil import copyfile
+
+#Operator List
+opList = [] 
+mutants = "+-*/"
+
+#Provide the path of the SUT and mutant library
+sutPath = 'sut.txt'
+mutantLibraryPath = 'mutantLibrary.txt'
+
+#create a folder in the directory of this python script where the SUT with injected mutants will be saved 
+if not os.path.exists('sutFolder'):
+    os.mkdir('sutFolder')
+
+#open the mutant library file
+mutantLibrary = open(mutantLibraryPath)
+
+#variables used to keep track of the mutant and its line number on the previous line of the mutant library  
+prevLineNumber = 0
+prevOperator = ""
+opratorLineCount = 0
+
+#iterate through each line of the mutant library 
+libraryLineNumber = 0 #line number
+for line in mutantLibrary:
+    libraryLineNumber = libraryLineNumber + 1
+
+    #Get the line number of the mutant in the SUT 
+    sutLineNumber =  line.split(":")[0]
+
+    #Get the original operator
+    mutantList = []
+    for char in line: 
+        if char in mutants:
+            mutantList.append(char)
+    originalOperator = mutantList[0]
+
+    #Discard mutantList for the last three lines of the mutant library (where the stats are displayed)
+    if len(mutantList) == 1:
+        mutantList = []
+    
+    #List of possible mutants from the mutant library generated in assignment 1
+    mutantList = mutantList[1:]
+
+    #Check if the previous operator was the same operator and on the same line of the SUT
+    #If it is the case, increment operatorLineCount 
+    if originalOperator == prevOperator and sutLineNumber == prevLineNumber:
+        operatorLineCount = operatorLineCount + 1
+    else:
+        operatorLineCount = 0
+
+    #Loop through the list of possible mutants, make a copy of the SUT and inject the mutants
+    mutantCounter = 0
+    for operator in mutantList:
+        #make a copy of the SUT 
+        newProgramPath ='sutFolder/'+ str(libraryLineNumber)+ "_"+str(mutantCounter)+'test.txt' 
+        newProgram = open(newProgramPath, "w")
+
+        #open the orriginal SUT
+        sut = open(sutPath)
+
+        #copy each line of code from the original SUT into the copy and inject the mutant in the right line of code
+        lineCounterMutant = 0
+        for mutantLine in sut: 
+            lineCounterMutant = lineCounterMutant + 1
+            if str(lineCounterMutant) == sutLineNumber:
+                mutantIndex =  [i for i, ltr in enumerate(mutantLine) if ltr == str(originalOperator)]
+                mutantIndex = mutantIndex[operatorLineCount]
+                mutantLine = list(mutantLine)
+                mutantLine[mutantIndex] = mutantList[mutantCounter]
+                mutantLine =  ''.join([str(string) for string in mutantLine])
+                newProgram.write(mutantLine)
+            else: 
+                newProgram.write(mutantLine)
+        newProgram.close
+        mutantCounter = mutantCounter +1
+
+    #Update the previous line number and operator
+    prevLineNumber = sutLineNumber
+    prevOperator = originalOperator
+
+
+
+
+
