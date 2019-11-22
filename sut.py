c8893233b687b232203668132b175cdb50205f5e
tbutch <tristan.bouchard@mail.mcgill.ca>
2019-11-22 12:42:42 -0500

    Modify SUT to throw exceptions on failure instead of exiting.
    Now takes two integer arguments as input to perform test, which will make it easy to pass our input vector

--- sut.py
+++ sut.py
@@ -1,10 +1,28 @@
+import sys
+import os
+
 def main():
     answer = sutProgram()
     print(answer)
 
 def sutProgram():
-    a = 1
-    b = 1
+    if(len(sys.argv) != 3):
+        print("\nThis program takes 2 integer arguments: \n" +
+           "\tArgument 1: Value for \"a\" \n" +
+           "\tArgument 2: Value for \"a\" \n" +
+           "\n\tExample command: python sut.py 3 2")
+        raise SystemExit("InvalidArgumentException - Must have two arguments")
+    else:
+        # Parse to make sure arguments are integers
+        for i in [1, 2]:
+            try:
+                arg = int(sys.argv[i])
+            except ValueError:
+                print("Argument is not an integer. Please enter an integer and try again")
+                raise SystemExit("InvalidArgumentException - Arguments must be integers")
+        
+    a = int(sys.argv[1])
+    b = int(sys.argv[2])
 
     c = (a * b) + 2 + 3
     d = c - b
