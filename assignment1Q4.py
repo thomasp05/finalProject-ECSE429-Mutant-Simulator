8360c798e6b47b6e1e054cf9323b08797869d47c
Thomas Philippon <thomas.philippon@mail.mcgill.ca>
2019-11-22 14:49:08 -0500

    Merge sort SUT

--- assignment1Q4.py
+++ assignment1Q4.py
@@ -6,14 +6,15 @@ __email__ = "thomas.philippon@mail.mcgill.ca"
 mutantList = ["+", "-", "*", "/"] #list of possible mutants
 nbMutant = [0,0,0,0]  #list containing the total number of mutants for each type
 output = open("mutantLibrary.txt", "w") #Output file containing the information about the mutants
-sut = open("sut.py") #Software Under Test.
+sut = open("sut_merge-sort.py") #Software Under Test.
 lineCounter = 0 #line number
 
 for line in sut:
     lineCounter = lineCounter + 1
     lineHistory = "" #string used to keep an history of what is on the line so we can avoid comments
     for char in line:
-        if (not "#" in lineHistory) and(char =='+' or char =='-' or char =='*' or char =='/'):
+        
+        if (not(("print" in lineHistory) or ("#" in lineHistory))) and (char =='+' or char =='-' or char =='*' or char =='/'):
             lineNumber = str(lineCounter)
             output.write( lineNumber + ": " + str(char) +  ", ")
             for i in mutantList:
@@ -25,6 +26,7 @@ for line in sut:
             
         else: 
             lineHistory = lineHistory+ char
+            print(lineHistory)
 
 #Print the information about the total number of mutants of each type
 output.write("Number of mutants of type + : " + str(nbMutant[0]) + "\n")
---
+++ sut_merge-sort.py
@@ -0,0 +1,78 @@
+import sys
+import os
+
+def main():
+    answer = sutProgram()
+    print(answer)
+
+
+# This code was taken from the website Geeks for Geeks 
+# https://www.geeksforgeeks.org/python-program-for-binary-search/
+# Returns index of x in arr if present, else -1 
+def binarySearch (arr, l, r, x): 
+  
+    # Check base case 
+    if r >= l: 
+  
+        mid = int(l + (r - l)/2)
+
+        # If element is present at the middle itself 
+        if arr[mid] == x: 
+            return mid 
+          
+        # If element is smaller than mid, then it can only 
+        # be present in left subarray 
+        elif arr[mid] > x: 
+            return binarySearch(arr, l, mid-1, x) 
+  
+        # Else the element can only be present in right subarray 
+        else: 
+            return binarySearch(arr, mid+1, r, x) 
+  
+    else: 
+        # Element is not present in the array 
+        return -1
+
+
+def sutProgram():
+    if(len(sys.argv) < 10):
+        print("\nThis program takes a list of integer arguments seperated by white spaces: \n" +
+           "\n\tExample command: python sut.py 3 2 2 3 4 5")
+        print("-1")
+    else:
+        # Parse to make sure arguments are integers
+        for i in range(1, len(sys.argv)):
+            try:
+                arg = int(sys.argv[i])
+            except ValueError:
+                print("Argument is not an integer. Please enter an integer and try again")
+                print("-1")
+        
+    # a = int(sys.argv[1])
+    # b = int(sys.argv[2])
+
+    myList = []
+    for item in sys.argv[1:]: 
+        myList.append(int(item))
+
+    arr = myList[:-1]
+    r = len(myList[:-1])
+    x = myList[-1]
+
+    #check if array is sorted and call binary search if it is. Return -1 if it is not 
+    flag = 0
+    i = 1
+    while i < len(arr): 
+        if(arr[i] < arr[i - 1]): 
+            flag = 1
+        i += 1  
+    if flag == 1: 
+        return -1
+    else:
+        f = binarySearch( arr, 0, r, x)
+        return f
+
+if __name__ == '__main__':
+    main()
+
+
